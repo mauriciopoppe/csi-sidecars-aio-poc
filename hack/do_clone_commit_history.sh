2a27a09daf9b59fa78abe36fa209b1a150041f35
Mauricio Poppe <mauricio.poppe@gmail.com>
2025-09-07 12:17:33 -0400

    Script to clone a repo with all its history included.
    The strategy is in this doc
    https://docs.google.com/document/d/1z7OU79YBnvlaDgcvmtYVnUAYFX1w9lyrgiPTV7RXjHM/edit?tab=t.0#bookmark=id.9rvah4b4vpty

--- hack/do_clone_commit_history.sh
+++ hack/do_clone_commit_history.sh
@@ -1,19 +1,56 @@
 #!/bin/bash
 set -euxo pipefail
 
+if [[ -z $VIRTUAL_ENV ]]; then
+  echo "This script must run within a virtual env"
+  echo "  python3 -m venv venv && source venv/bin/activate "
+  exit 1
+fi
+
+if [[ -z $(git filter-repo --version) ]]; then
+  echo "git-filter-repo is required, installing it..."
+  pip install git-filter-repo
+fi
+
+trash tmp
+if [[ -d tmp ]]; then
+  echo "tmp directory exists, remove it to start from scratch"
+  exit 1
+fi
+
 # Create a temporary directory for the clone
 mkdir -p tmp
 
-# Get all the hashes from csi-release-tools that we should ignore
-git clone https://github.com/kubernetes-csi/csi-release-tools tmp/csi-release-tools
-(cd tmp/csi-release-tools && git rev-list --reverse HEAD) > hack/csi-release-tools-hashes.txt
+# Let's assume that this repo is not the target of the merge apply
+# so that the history isn't polluted.
+#
+# Instead, let's do it in a temporary new git repo as a POC.
+mkdir -p tmp/csi-sidecars
+(cd tmp/csi-sidecars && git init)
 
-# Clone the full history of the repository
-git clone https://github.com/kubernetes-csi/external-attacher tmp/external-attacher
-# Create the directory to store the patches
-mkdir -p hack/patches/attacher
-# Generate the patches for all commits on the master branch
-(cd tmp/external-attacher && git format-patch --root --full-index -o ../../hack/patches/attacher)
-# Clean up the temporary clone
-rm -rf tmp/external-attacher
+# Clone the full history of each repository
+for i in attacher,master provisioner,master resizer,master; do
+  IFS=',' read SIDECAR SIDECAR_HASH <<<"${i}"
 
+  git clone https://github.com/kubernetes-csi/external-${SIDECAR} tmp/${SIDECAR}
+  (
+    cd tmp/${SIDECAR}
+    git status
+    git checkout ${SIDECAR_HASH}
+    git rev-parse --short HEAD
+    # --force is required if we checkout to a different branch.
+    # alternatively, comment the `git checkout` about and remove the flag.
+    git filter-repo --to-subdirectory-filter pkg/${SIDECAR} --force
+  )
+
+  # Import a ${SIDECAR} repo into the tmp/csi-sidecars repo
+  (
+    cd tmp/csi-sidecars
+    git remote add external-${SIDECAR} ../${SIDECAR}
+    git fetch external-${SIDECAR}
+    git merge external-${SIDECAR}/${SIDECAR_HASH} --allow-unrelated-histories --no-edit
+  )
+done
+
+echo "Complete!"
+echo "Check the new repo at tmp/csi-sidecars"
