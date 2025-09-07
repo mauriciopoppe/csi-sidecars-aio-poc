a194649b84436b050cc7ed62bd57ad6ed6f2c11c
Mauricio Poppe <mauricio.poppe@gmail.com>
2025-09-07 14:31:12 -0400

    Add external repo commit details when importing commits

--- hack/do_clone_commit_history.sh
+++ hack/do_clone_commit_history.sh
@@ -28,26 +28,41 @@ mkdir -p tmp/csi-sidecars
 (cd tmp/csi-sidecars && git init)
 
 # Clone the full history of each repository
-for i in attacher,master provisioner,master resizer,master; do
+for i in external-attacher,master external-provisioner,master external-resizer,master; do
   IFS=',' read SIDECAR SIDECAR_HASH <<<"${i}"
 
-  git clone https://github.com/kubernetes-csi/external-${SIDECAR} tmp/${SIDECAR}
+  git clone https://github.com/kubernetes-csi/${SIDECAR} tmp/${SIDECAR}
   (
     cd tmp/${SIDECAR}
     git status
     git checkout ${SIDECAR_HASH}
     git rev-parse --short HEAD
+
     # --force is required if we checkout to a different branch.
     # alternatively, comment the `git checkout` about and remove the flag.
-    git filter-repo --to-subdirectory-filter pkg/${SIDECAR} --force
+    git filter-repo \
+      --commit-callback "
+# commit.original_id is a byte string of the original SHA-1
+original_hash = commit.original_id.decode()
+
+# Github link for reference.
+github_link = f\"https://github.com/kubernetes-csi/${SIDECAR}/commit/{original_hash}\"
+
+# Append a trailer to the commit message.
+# We decode the message to add the string, then re-encode to bytes.
+original_message = commit.message.decode()
+new_message = f\"{original_message}\n\nImported-from: ${SIDECAR}\n\nOriginal-commit: {original_hash}\n\nGithub-link: {github_link}\"
+commit.message = new_message.encode()
+    " \
+      --to-subdirectory-filter pkg/${SIDECAR#external-} --force
   )
 
   # Import a ${SIDECAR} repo into the tmp/csi-sidecars repo
   (
     cd tmp/csi-sidecars
-    git remote add external-${SIDECAR} ../${SIDECAR}
-    git fetch external-${SIDECAR}
-    git merge external-${SIDECAR}/${SIDECAR_HASH} --allow-unrelated-histories --no-edit
+    git remote add ${SIDECAR} ../${SIDECAR}
+    git fetch ${SIDECAR}
+    git merge ${SIDECAR}/${SIDECAR_HASH} --allow-unrelated-histories --no-edit
   )
 done
 
